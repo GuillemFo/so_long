e5caae12923818f0624202bdadd0fda532abc6aa
Guillem Forns Sant <gforns-s@cbr2s5.42barcelona.com>
2023-11-12 13:27:43 +0100

    tools to print the matrix

---
+++ tools.c
@@ -0,0 +1,22 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tools.c                                            :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: gforns-s <gforns-s@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/11/12 13:13:39 by gforns-s          #+#    #+#             */
+/*   Updated: 2023/11/12 13:13:57 by gforns-s         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "./Include/so_long.h"
+
+void print_matrix(char **matrix, int rows, int cols) {
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            ft_printf("%c ", matrix[i][j]);
+        }
+        ft_printf("\n");
+    }
+}
